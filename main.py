e6c2c59feef7b888a7095b8a67675f658bca919d
Emerson Filho <emerson.dev.machado@gmail.com>
2023-02-09 21:40:15 -0300

    feat: create script execution flow

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def download(output_path: str, url: str, file_extension: str) -> YouTube:
     streams.download(output_path=output_path, filename=f"{youtube.title}.{file_extension}")
     return youtube
 
+if __name__ == "__main__":
+    url = input("Insert the url: ")
+    output_path = input(f"Insert the download path (or \"enter\" for default - \"{DEFAULT_DOWNLOAD_PATH_WINDOWS}\"): ")
+    file_extension = input("Insert the file extension (\"mp3\" or \"mp4\"): ")
+    download(output_path or DEFAULT_DOWNLOAD_PATH_WINDOWS, url, file_extension)
