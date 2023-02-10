263f4437b5cc4ac536d604305fa04776a4bdcd50
Emerson Filho <emerson.dev.machado@gmail.com>
2023-02-09 21:38:04 -0300

    feat: create download function

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import os
+from pytube import YouTube
 
 DEFAULT_DOWNLOAD_PATH_WINDOWS = os.path.expanduser('~') + "\\Downloads\\"
+
+def download(output_path: str, url: str, file_extension: str) -> YouTube:
+    youtube = YouTube(url)
+    streams = youtube.streams.get_highest_resolution()
+    streams.download(output_path=output_path, filename=f"{youtube.title}.{file_extension}")
+    return youtube
 
